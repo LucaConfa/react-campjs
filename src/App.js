aa4e6974be4b77954a797ea2563ef5e33340533b
jgan91 <jgan91@gmail.com>
2019-09-09 17:12:39 +1000

    front end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import request from 'request';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import './App.css';
+
+const baseUrl = 'http://192.168.1.97:3003';
+
+const mockPlayers = [
+  {
+    name: 'justin',
+    id: '1',
+  },
+  {
+    name: 'luca',
+    id: '2',
+  },
+  {
+    name: 'pura',
+    id: '3',
+  },
+  {
+    name: 'xtian',
+    id: '4',
+  }
+];
+
+const makePlayerSelectList = ({title, name, players, input, eventHandlers, score}) => {
+  const playerOptions = players.map(player => 
+    <option
+      key={player.id}
+      value={player.name}
+    >
+      {toSentenceCase(player.name)}
+    </option>
+  );
+
+  return <>
+    <h2>{title}</h2>
+    <button
+      name={name}
+      onClick={eventHandlers.decrement}
+    >
+      -
+    </button>
+    {score}
+    <button
+      name={name}
+      onClick={eventHandlers.increment}
+    >
+      +
+    </button>
+    <select
+      name={name}
+      multiple
+      onChange={eventHandlers.handlePlayerSelect}
+    >
+      {playerOptions}
+    </select>
+    <input
+      type='text'
+      name={name}
+      label='Name'
+      onChange={eventHandlers.handleInput}
+      value={input}
+    />
+    <button
+      name={name}
+      onClick={eventHandlers.createPlayer}
+    >
+      +
+    </button>
+  </>
+}
+
+const toSentenceCase = string =>
+  string.length > 0
+    ? string.replace(string[0], string[0].toUpperCase())
+    : '';
 
 // Some folks find value in a centralized route config.
 // A route config is just data. React is great at mapping
@@ -8,8 +84,122 @@ import './App.css';
 
 ////////////////////////////////////////////////////////////
 // first our route components
-function Sandwiches() {
-  return <h2>Sandwiches</h2>;
+function Match() {
+  const [values, setValues] = useState({
+    // allPlayers: [],
+    allPlayers: mockPlayers,
+    inputPlayer1: '',
+    inputPlayer2: '',
+    matchHistory: [],
+    player1: '',
+    player2: '',
+    scorePlayer1: 0,
+    scorePlayer2: 0,
+  });
+
+  // useEffect(() => {
+  //   request.get(`${baseUrl}/player`, {json: true}, (err, res, body) => {
+  //     setValues({...values, allPlayers: body});
+  //   });
+  // }, []);
+
+  const createPlayer = e => {
+    const { name } = e.target;
+    const player = values[`input${toSentenceCase(name)}`]
+
+    const params = {
+      method: 'POST',
+      uri: `${baseUrl}/player`,
+      json: true,
+      body: {
+        name: player,
+      },
+    };
+
+    request(
+      params,
+      (err, res, body) => {
+        if (err) {
+          console.log(err);
+        }
+
+        console.log(res.status);
+        console.log(body); 
+      }
+    );
+    // values.allPlayers.push(values[`input${toSentenceCase(name)}`]);
+    // setValues({...values, allPlayers: values.allPlayers});
+  };
+
+  const handleInput = e => {
+    const {name, value} = e.target
+    const updatedValue = `input${toSentenceCase(name)}`;
+    setValues({...values, [updatedValue]: value})
+  };
+
+  const handleMatchSubmit = () => {
+    // setValues({...values, matchHistory: 
+  };
+
+  const handlePlayerSelect = e => {
+    const { name, value } = e.target;
+    setValues({...values, [name]: value });
+  };
+
+  const decrement = e => {
+    const {name} = e.target;
+    const updatedValue = `score${toSentenceCase(name)}`;
+    if (values[updatedValue] === 0) { return };
+    setValues({...values, [updatedValue]: values[updatedValue] - 1})
+  };
+
+
+  const increment = e => {
+    const {name} = e.target
+    const updatedValue = `score${toSentenceCase(name)}`;
+    setValues({...values, [updatedValue]: values[updatedValue] + 1})
+  };
+
+  const eventHandlers = {
+    decrement,
+    createPlayer,
+    handleInput,
+    handleMatchSubmit,
+    handlePlayerSelect,
+    increment,
+  };
+
+  const selectListPlayer1 = makePlayerSelectList({
+      title: "Player 1",
+      name: "player1",
+      input: values.inputPlayer1,
+      players: values.allPlayers,
+      score: values.scorePlayer1,
+      eventHandlers,
+  })
+
+  const selectListPlayer2 = makePlayerSelectList({
+      title: "Player 2",
+      name: "player2",
+      input: values.inputPlayer2,
+      players: values.allPlayers,
+      score: values.scorePlayer2,
+      eventHandlers,
+  })
+
+  const matchSubmit = (
+    <button onClick={handleMatchSubmit}>
+      Submit Match
+    </button>
+  )
+
+  return (
+    <>
+      {selectListPlayer1}
+      {selectListPlayer2}
+      {matchSubmit}
+    </>
+  );
 }
 
 function Tacos({ routes }) {
@@ -44,8 +234,8 @@ function Cart() {
 // then our route config
 const routes = [
   {
-    path: "/sandwiches",
-    component: Sandwiches
+    path: "/match",
+    component: Match
   },
   {
     path: "/tacos",
@@ -86,7 +276,7 @@ function RouteConfig() {
             <Link to="/tacos">Tacos</Link>
           </li>
           <li>
-            <Link to="/sandwiches">Sandwiches</Link>
+            <Link to="/match">Match</Link>
           </li>
         </ul>
 
